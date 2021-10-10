24a1650320cb5a3738ab9c9828e480c1d93ab0fc
Adam Johnston <adamjohnston@Adams-MacBook-Pro.local>
2021-10-09 23:44:59 -0600

    updated methods with orchestration service using helm

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/thinksystemio/package-response"
+	response "github.com/thinksystemio/package-response"
 )
 
 func main() {
@@ -22,61 +22,62 @@ func main() {
 	r.Get("/loading", Loading)
 	r.Get("/loading/*", Loading)
 
-	// Cluster
-	r.With(Middleware).Get("/{cluster}", Loading)
-	r.With(Middleware).Get("/{cluster}/*", Loading)
+	// Instance
+	r.With(Middleware).Get("/{instance}", Loading)
+	r.With(Middleware).Get("/{instance}/*", Loading)
 
 	// Frontend
-	r.With(Middleware).Get("/{cluster}/{app}", Frontend)
-	r.With(Middleware).Get("/{cluster}/{app}/*", Frontend)
+	r.With(Middleware).Get("/{instance}/{app}", Frontend)
+	r.With(Middleware).Get("/{instance}/{app}/*", Frontend)
 
 	// Backend
-	r.With(Middleware).Get("/{cluster}/{app}/api", Backend)
-	r.With(Middleware).Get("/{cluster}/{app}/api/*", Backend)
-	r.With(Middleware).Post("/{cluster}/{app}/api", Backend)
-	r.With(Middleware).Post("/{cluster}/{app}/api/*", Backend)
+	r.With(Middleware).Get("/{instance}/{app}/api", Backend)
+	r.With(Middleware).Get("/{instance}/{app}/api/*", Backend)
+	r.With(Middleware).Post("/{instance}/{app}/api", Backend)
+	r.With(Middleware).Post("/{instance}/{app}/api/*", Backend)
 
-	http.ListenAndServe(":81", r)
+	http.ListenAndServe(":80", r)
 }
 
 // Echo allows pinging of this service
 func Echo(w http.ResponseWriter, r *http.Request) {
-	bg := GetBackground(r)
-	bg.Res.SendDataWithStatusCode(w, "echo", http.StatusOK)
+	appContext := GetAppContext(r)
+	appContext.Res.SendDataWithStatusCode(w, "echo", http.StatusOK)
 }
 
 // NotFound redirects to the not found page
 func NotFound(w http.ResponseWriter, r *http.Request) {
-	bg := GetBackground(r)
-	bg.Res.SendDataWithStatusCode(w, "not found", http.StatusNotFound)
+	appContext := GetAppContext(r)
+	appContext.Res.SendDataWithStatusCode(w, "not found", http.StatusNotFound)
 }
 
+// Parse instance and app, place into context
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		bg := &Background{
-			Cluster: chi.URLParam(r, "cluster"),
-			App:     chi.URLParam(r, "app"),
-			Res:     response.CreateResponse(),
+		appContext := &AppContext{
+			Instance: chi.URLParam(r, "instance"),
+			App:      chi.URLParam(r, "app"),
+			Res:      response.CreateResponse(),
 		}
 
-		if ok := VerifyCluster(bg); !ok {
-			target := fmt.Sprintf("http://%s/%s", r.Host, bg.Cluster)
+		if ok := VerifyInstance(appContext); !ok {
+			target := fmt.Sprintf("http://%s/%s", r.Host, appContext.Instance)
 			http.Redirect(w, r, target, http.StatusTemporaryRedirect)
 			return
 		}
 
-		ctx := context.WithValue(r.Context(), BackgroundKey, bg)
+		ctx := context.WithValue(r.Context(), AppContextKey, appContext)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
 func Loading(w http.ResponseWriter, r *http.Request) {
-	bg := GetBackground(r)
+	appContext := GetAppContext(r)
 
-	target, err := LoadingAppURL(bg)
+	target, err := LoadingAppURL(appContext)
 	target.Path = SkipNPathParams(r.URL.Path, 1)
 	if err != nil {
-		bg.Res.SendErrorWithStatusCode(w, err, http.StatusInternalServerError)
+		appContext.Res.SendErrorWithStatusCode(w, err, http.StatusInternalServerError)
 		return
 	}
 
@@ -84,26 +85,26 @@ func Loading(w http.ResponseWriter, r *http.Request) {
 }
 
 func Frontend(w http.ResponseWriter, r *http.Request) {
-	bg := GetBackground(r)
+	appContext := GetAppContext(r)
 
-	target, err := FrontendAppURL(bg)
+	target, err := FrontendAppURL(appContext)
 	target.Path = SkipNPathParams(r.URL.Path, 2)
 	if err != nil {
-		bg.Res.SendErrorWithStatusCode(w, err, http.StatusInternalServerError)
+		appContext.Res.SendErrorWithStatusCode(w, err, http.StatusInternalServerError)
 		return
 	}
 
 	status, err := PingService(target)
 	if status == http.StatusBadGateway && strings.Contains(r.URL.Path, "activate") {
-		res, err := RequestWithTimeout(FrontendDeployURL(bg))
+		res, err := RequestWithTimeout(FrontendDeployURL(appContext))
 		if err != nil {
-			bg.Res.AppendError(err)
+			appContext.Res.AppendError(err)
 		}
-		bg.Res.SendWithStatusCode(w, res.StatusCode)
+		appContext.Res.SendWithStatusCode(w, res.StatusCode)
 		return
 	}
 	if err != nil {
-		bg.Res.SendErrorWithStatusCode(w, err, status)
+		appContext.Res.SendErrorWithStatusCode(w, err, status)
 		return
 	}
 
@@ -111,27 +112,27 @@ func Frontend(w http.ResponseWriter, r *http.Request) {
 }
 
 func Backend(w http.ResponseWriter, r *http.Request) {
-	bg := GetBackground(r)
+	appContext := GetAppContext(r)
 
-	target, err := BackendAppURL(bg)
+	target, err := BackendAppURL(appContext)
 	target.Path = SkipNPathParams(r.URL.Path, 2)
 	if err != nil {
-		bg.Res.SendErrorWithStatusCode(w, err, http.StatusInternalServerError)
+		appContext.Res.SendErrorWithStatusCode(w, err, http.StatusInternalServerError)
 		return
 	}
 
 	status, err := PingService(target)
 	if status == http.StatusBadGateway && strings.Contains(r.RequestURI, "activate") {
-		res, err := RequestWithTimeout(BackendDeployURL(bg))
+		res, err := RequestWithTimeout(BackendDeployURL(appContext))
 		if err != nil {
-			bg.Res.SendErrorWithStatusCode(w, err, http.StatusInternalServerError)
+			appContext.Res.SendErrorWithStatusCode(w, err, http.StatusInternalServerError)
 			return
 		}
-		bg.Res.SendWithStatusCode(w, res.StatusCode)
+		appContext.Res.SendWithStatusCode(w, res.StatusCode)
 		return
 	}
 	if err != nil {
-		bg.Res.SendErrorWithStatusCode(w, err, status)
+		appContext.Res.SendErrorWithStatusCode(w, err, status)
 		return
 	}
 
--- utils.go
+++ utils.go
@@ -9,123 +9,18 @@ import (
 	"strings"
 	"time"
 
-	"github.com/thinksystemio/package-response"
+	response "github.com/thinksystemio/package-response"
 )
 
-type key int
+//
+// HTTP Helpers
+//
 
-const (
-	BackgroundKey key = iota
-)
-
-var (
-	registry = map[string]struct{}{}
-)
-
-type Background struct {
-	Cluster string
-	App     string
-	Res     *response.Response
-}
-
-func GetBackground(r *http.Request) *Background {
-	value, ok := r.Context().Value(BackgroundKey).(*Background)
-	if !ok {
-		return &Background{Res: response.CreateResponse()}
-	}
-	return value
-}
-
-func VerifyCluster(background *Background) bool {
-	_, ok := registry[background.Cluster]
-	if !ok {
-		registry[background.Cluster] = struct{}{}
-		http.Get(ClusterDeployURL(background))
-	}
-	return ok
-}
-
-func ClusterDeployURL(background *Background) string {
-	return fmt.Sprintf(
-		"http://service-backend-orchestration:81/%s",
-		background.Cluster,
-	)
-}
-
-func LoadingAppURL(background *Background) (*url.URL, error) {
-	URL := fmt.Sprintf(
-		"http://service-frontend-loading:80/%s",
-		background.Cluster,
-	)
-	return url.Parse(URL)
-}
-
-func FrontendAppURL(background *Background) (*url.URL, error) {
-	URL := fmt.Sprintf(
-		"http://%s-service-frontend-%s:80",
-		background.Cluster,
-		background.App,
-	)
-	return url.Parse(URL)
-}
-
-func FrontendDeployURL(background *Background) string {
-	return ThinksystemContainer(
-		background.Cluster,
-		"frontend",
-		background.App,
-		"80")
-}
-
-func BackendAppURL(background *Background) (*url.URL, error) {
-	URL := fmt.Sprintf(
-		"http://%s-service-backend-%s:81",
-		background.Cluster,
-		background.App,
-	)
-	return url.Parse(URL)
-}
-
-func BackendDeployURL(background *Background) string {
-	return ThinksystemContainer(
-		background.Cluster,
-		"backend",
-		background.App,
-		"81")
-}
-
-func ThinksystemContainer(clusterName, appType, appName, appPort string) string {
-	return fmt.Sprintf("http://service-backend-orchestration:81/%s/%s/%s/%s",
-		clusterName,
-		appType,
-		appName,
-		appPort,
-	)
-}
-
-func RemoteContainer(clusterName, appType, appName, appPort, image, imageName string) string {
-	return fmt.Sprintf("http://service-backend-orchestration:81/%s/%s/%s/%s/%s/%s",
-		clusterName,
-		appType,
-		appName,
-		appPort,
-		image,
-		imageName,
-	)
-}
-
-func PingService(service *url.URL) (int, error) {
-	res, err := http.Get(fmt.Sprintf("http://%s/%s", service.Host, "echo"))
-	if err != nil {
-		return http.StatusBadGateway, err
-	}
-	return res.StatusCode, err
-}
-
+// ProxyRequest proxies a request to a given instance.
 func ProxyRequest(w http.ResponseWriter, r *http.Request, target *url.URL) {
 	reverseProxy := httputil.ReverseProxy{
 		Director: func(r *http.Request) {
-			r.Header.Add("Cluster", strings.Split(target.Host, "-")[0])
+			r.Header.Add("Instance", strings.Split(target.Host, "-")[0])
 			r.URL.Scheme = target.Scheme
 			r.URL.Host = target.Host
 			r.URL.Path = target.Path
@@ -134,6 +29,8 @@ func ProxyRequest(w http.ResponseWriter, r *http.Request, target *url.URL) {
 	reverseProxy.ServeHTTP(w, r)
 }
 
+// RequestWithTimeout sends a request with a given context that
+// will timeout.
 func RequestWithTimeout(url string) (*http.Response, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
@@ -150,6 +47,19 @@ func RequestWithTimeout(url string) (*http.Response, error) {
 	return res, nil
 }
 
+// PingService checks is a service has been deployed
+// for a given instance.
+func PingService(service *url.URL) (int, error) {
+	res, err := http.Get(fmt.Sprintf("http://%s/%s", service.Host, "echo"))
+	if err != nil {
+		return http.StatusBadGateway, err
+	}
+	return res.StatusCode, err
+}
+
+// SkipNPathParams trims the first n items in a path. For examples,
+// if the path is test/route/path with n=1, it will return
+// route/path.
 func SkipNPathParams(path string, n int) string {
 	if n < 0 {
 		return "/"
@@ -166,4 +76,100 @@ func SkipNPathParams(path string, n int) string {
 	}
 
 	return "/"
+}
+
+//
+// Deployment Helpers
+//
+
+var (
+	registry = map[string]struct{}{}
+)
+
+type key int
+
+const (
+	AppContextKey key = iota
+)
+
+type AppContext struct {
+	Instance string
+	App      string
+	Res      *response.Response
+}
+
+// GetAppContext gets the current context being passed
+// through the application
+func GetAppContext(r *http.Request) *AppContext {
+	value, ok := r.Context().Value(AppContextKey).(*AppContext)
+	if !ok {
+		return &AppContext{Res: response.CreateResponse()}
+	}
+	return value
+}
+
+// VerifyInstance ensures that the registry has an instance
+// already created. If the instance is not already created,
+// it will create one with all the deployments.
+func VerifyInstance(appContext *AppContext) bool {
+	_, ok := registry[appContext.Instance]
+	if !ok {
+		registry[appContext.Instance] = struct{}{}
+		http.Get(DeployInstance(appContext))
+	}
+	return ok
+}
+
+// DeployInstance sends a request to the orchestration
+// server and tells it to create a new instance with
+// all of the necessary containers.
+func DeployInstance(appContext *AppContext) string {
+	return fmt.Sprintf(
+		"http://service-backend-orchestration:80/%s",
+		appContext.Instance,
+	)
+}
+
+func LoadingAppURL(appContext *AppContext) (*url.URL, error) {
+	URL := fmt.Sprintf(
+		"http://service-frontend-loading:80/%s",
+		appContext.Instance,
+	)
+	return url.Parse(URL)
+}
+
+func FrontendAppURL(appContext *AppContext) (*url.URL, error) {
+	URL := fmt.Sprintf(
+		"http://%s-service-frontend-%s:80",
+		appContext.Instance,
+		appContext.App,
+	)
+	return url.Parse(URL)
+}
+
+func FrontendDeployURL(appContext *AppContext) string {
+	URL := fmt.Sprintf(
+		"http://service-backend-orchestration:80/%s/%s/frontend",
+		appContext.Instance,
+		appContext.App,
+	)
+	return URL
+}
+
+func BackendAppURL(appContext *AppContext) (*url.URL, error) {
+	URL := fmt.Sprintf(
+		"http://%s-service-backend-%s:81",
+		appContext.Instance,
+		appContext.App,
+	)
+	return url.Parse(URL)
+}
+
+func BackendDeployURL(appContext *AppContext) string {
+	URL := fmt.Sprintf(
+		"http://service-backend-orchestration:80/%s/%s/backend",
+		appContext.Instance,
+		appContext.App,
+	)
+	return URL
 }
