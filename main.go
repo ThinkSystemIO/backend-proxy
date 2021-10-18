856fd8768567a0d50ecab2b9f4dab6f6ee43de95
ThinkSystemIO <adamjohnston@Adams-MBP.murray>
2021-10-17 23:09:08 -0600

    added return if failure

--- main.go
+++ main.go
@@ -68,7 +68,9 @@ func Middleware(next http.Handler) http.Handler {
 					errors.New("failed to deploy instance"),
 					http.StatusInternalServerError,
 				)
+				return
 			}
+
 			Loading(w, r)
 			return
 		}
